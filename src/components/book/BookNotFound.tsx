7c46f01ef09d92a11bd8e9e159ba2ed907f0bdc2
jhoneber31 <jhoneber31@gmail.com>
2024-12-13 13:50:14 -0500

    [UPDATE] book not found

--- src/components/book/BookNotFound.tsx
+++ src/components/book/BookNotFound.tsx
@@ -4,7 +4,7 @@ export const BookNotFound = () => {
   return (
     <div className="flex flex-col justify-center gap-y-5 items-center h-[calc(100vh-260px)]">
       <SadIcon/>
-      <h4 className="text-blue-100 text-4xl font-semibold">No book found. Please try searching with a different keyword.</h4>
+      <h4 className="text-blue-100 text-4xl text-center font-semibold">No book found. Please try searching with a different keyword.</h4>
     </div>
   )
 }
